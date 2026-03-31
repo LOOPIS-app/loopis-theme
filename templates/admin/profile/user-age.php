606dfecbb393c0fbd1e99af334ef76077a9dacae
HubertHilborn <156682461+HubertHilborn@users.noreply.github.com>
2026-03-31 07:30:23 +0200

    Update user-age.php
    fixed

--- templates/admin/profile/user-age.php
+++ templates/admin/profile/user-age.php
@@ -6,16 +6,18 @@
  * $user_id has to be passed from context!
  */
  
-if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
+// Get user birthday
+$birthyear = get_user_meta($user_id, 'wpum_birthyear', true);
+$birthyear_int = intval($birthyear);
+// Get current year and calculate user's age
+if ($birthyear_int > 0) {
+    $current_year = intval(date('Y'));
+    $age = $current_year - $birthyear_int;
+    $output = "🚼 $birthyear_int ≈ $age år";
+} else {
+    $output = "🚼 Okänd";
 }
 
-// Uses $user_id passed from author.php
-$birthyear = get_user_meta($user_id, 'wpum_birthyear', true);
-
-// Calculate current year and user's age
-$current_year = date('Y');
-$age = $current_year - $birthyear;
 
 // Output
-echo esc_html('🚼 ' . $birthyear . ' ≈ ' . $age . ' år');
+echo esc_html($output);
