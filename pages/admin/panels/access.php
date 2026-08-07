433c7ba95c98922e185bbf25bfe95a28655d3a0d
joxyzan <johan.hagvil@gmail.com>
2026-08-07 17:10:43 +0200

    admin cleanup

--- pages/admin/panels/access.php
+++ pages/admin/panels/access.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Show statistics for comments in admin dashboard
+ * Show user with access to admin dashboard
  */
  
 if (!defined('ABSPATH')) {
@@ -8,29 +8,7 @@ if (!defined('ABSPATH')) {
 }
 
 ?>
-        <p class="small">Wordpress administrators:
-            <?php
-            $users = get_users(array('role' => 'administrator'));
-            foreach ($users as $user) {
-                $user_first_name = get_user_meta($user->ID, 'first_name', true);
-                $author_link = get_author_posts_url($user->ID);
-                echo '<a href="' . esc_url($author_link) . '">👽' . esc_html($user_first_name) . '</a> &nbsp;';
-            }
-            ?>
-        </p>
-
-        <p class="small">Develoopers:
-            <?php
-            $users = get_users(array('role' => 'develooper'));
-            foreach ($users as $user) {
-                $user_first_name = get_user_meta($user->ID, 'first_name', true);
-                $author_link = get_author_posts_url($user->ID);
-                echo '<a href="' . esc_url($author_link) . '">🧑‍💻' . esc_html($user_first_name) . '</a> &nbsp;';
-            }
-            ?>
-        </p>
-        
-        <p class="small">Admins (managers):
+        <p class="small">Local admin (manager):
             <?php
             $users = get_users(array('role' => 'manager'));
             foreach ($users as $user) {
@@ -42,7 +20,7 @@ if (!defined('ABSPATH')) {
             ?>
         </p>
         
-        <p class="small">Styrelsen:
+        <p class="small">Styrelse (board):
             <?php
             $users = get_users(array('role' => 'board'));
             foreach ($users as $user) {
@@ -50,6 +28,28 @@ if (!defined('ABSPATH')) {
                 $user_last_name = get_user_meta($user->ID, 'last_name', true);
                 $author_link = get_author_posts_url($user->ID);
                 echo '<a href="' . esc_url($author_link) . '">🕵' . esc_html($user_first_name . ' ' . $user_last_name) . '</a> &nbsp;';
+            }
+            ?>
+        </p>
+
+        <p class="small">Develooper (develooper):
+            <?php
+            $users = get_users(array('role' => 'develooper'));
+            foreach ($users as $user) {
+                $user_first_name = get_user_meta($user->ID, 'first_name', true);
+                $author_link = get_author_posts_url($user->ID);
+                echo '<a href="' . esc_url($author_link) . '">🧑‍💻' . esc_html($user_first_name) . '</a> &nbsp;';
+            }
+            ?>
+        </p>
+
+        <p class="small">WP-Admin (administrator):
+            <?php
+            $users = get_users(array('role' => 'administrator'));
+            foreach ($users as $user) {
+                $user_first_name = get_user_meta($user->ID, 'first_name', true);
+                $author_link = get_author_posts_url($user->ID);
+                echo '<a href="' . esc_url($author_link) . '">👽' . esc_html($user_first_name) . '</a> &nbsp;';
             }
             ?>
         </p>
