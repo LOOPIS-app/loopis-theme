342c7858903e9691fcd17d5ea2b407b4062bb0a2
joxyzan <johan.hagvil@gmail.com>
2026-08-25 11:25:48 +0200

    header & footer

--- footer.php
+++ footer.php
@@ -1,10 +1,6 @@
 </div><!--container-->
 <?php get_template_part('templates/general/scroll-to-top'); ?>
-
-<!-- Support form for on all pages except support posts -->
-<?php if ((current_user_can('member') || current_user_can('loopis_support')) && !is_singular('support')) : ?>
-    <?php get_template_part('templates/forms/support-form'); ?>
-<?php endif; ?>
+<?php get_template_part('templates/faq/questions-area'); ?>
 
 </div><!--wrapper-->
 
@@ -12,16 +8,12 @@
     <nav>
             <a href="<?php echo esc_url(home_url('/')); ?>" class="footer-item">
                 <span class="emoji">️🎁️</span>
-                <span class="text">Saker</span>
-            </a>
-            <a href="<?php echo esc_url(home_url('/?s=')); ?>" class="footer-item">
-                <span class="emoji">🔍️</span>
-                <span class="text">Sök</span>
+                <span class="text">Saker att få</span>
             </a>
 
-            <a href="<?php echo esc_url(home_url('/submit/')); ?>" class="footer-item">
-                <span class="emoji">💚</span>
-                <span class="text">Ge bort</span>
+            <a href="<?php echo esc_url(home_url('/area')); ?>" class="footer-item">
+                <span class="emoji">📍</span>
+                <span class="text"><?php echo get_bloginfo('name'); ?></span>
             </a>
 
             <?php if (is_user_logged_in()) : ?>
@@ -29,17 +21,8 @@
                     <span class="emoji">🔔</span>
                     <span class="text">Min aktivitet</span>
                 </a>
-
-                <a href="<?php echo esc_url(home_url('/user/')); ?>" class="footer-item">
-                    <span class="emoji">👤️</span>
-                    <span class="text">Min profil</span>
-                </a>
                 
             <?php else : ?>
-                <a href="<?php echo esc_url(network_home_url( '/faq/' )); ?>" class="footer-item">
-                    <span class="emoji">💡</span>
-                    <span class="text">Hur funkar det?</span>
-                </a>
 
                 <a href="<?php echo esc_url(get_loopis_login_url()); ?>" class="footer-item">
                     <span class="emoji">👤️</span>
@@ -49,7 +32,7 @@
     </nav>
 
 <?php if (current_user_can('loopis_admin') || current_user_can('manage_options')) : ?>
-    <div class="footer-backdoor" onclick="location.href='<?php echo esc_url(home_url('/admin/')); ?>'">🐙</div>
+    <div class="footer-backdoor" onclick="location.href='<?php echo esc_url(home_url('/admin/')); ?>'">🦀</div>
 <?php endif; ?>
 
 </footer><!--footer-->
--- templates/admin/footer-admin.php
+++ templates/admin/footer-admin.php
@@ -6,25 +6,15 @@
 <footer id="footer" class="footer-admin">
     <nav>
         <a href="<?php echo  esc_url(home_url('/admin/')); ?>" class="footer-item">
-        <span class="emoji">🐙</span>
+        <span class="emoji">🦀</span>
         <span class="text">Översikt</span>
-        </a>
-
-        <a href="<?php echo esc_url( add_query_arg('view', 'traffic-gifts', home_url('/admin/')) ); ?>" class="footer-item">
-        <span class="emoji">⏰</span>
-        <span class="text">Påminnelser</span>
         </a>
         
         <a href="<?php echo esc_url( add_query_arg('view', 'stats', home_url('/admin/')) ); ?>" class="footer-item">
         <span class="emoji">📊</span>
         <span class="text">Statistik</span>
         </a>
-
-        <a href="<?php echo esc_url( add_query_arg('view', 'support', home_url('/admin/')) ); ?>" class="footer-item">
-        <span class="emoji">🛟</span>
-        <span class="text">Support</span>
-        </a>
-        
+                
         <a href="<?php echo esc_url( add_query_arg('view', 'storage', home_url('/admin/')) ); ?>" class="footer-item">
         <span class="emoji">📦</span>
         <span class="text">Lager</span>
