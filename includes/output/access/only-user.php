5ba5b528bbc83fe477ef0b7d80f5de3bfbd46fe0
joxyzan <johan.hagvil@gmail.com>
2026-08-26 17:39:29 +0200

    launch fixes

--- includes/output/access/only-user.php
+++ includes/output/access/only-user.php
@@ -11,5 +11,3 @@ if (!defined('ABSPATH')) {
 	<p>🚧 Du har inte behörighet att se denna sida.</p>
 	<p><span class="big-link"><?php get_template_part('templates/links/go-back'); ?></span></p>
 </div>
-
-<?php get_template_part('templates/links/log-in-button'); ?>
