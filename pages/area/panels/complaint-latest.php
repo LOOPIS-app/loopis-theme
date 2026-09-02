56cea51ebf5a8b2c01121cee9242b7c64b9145f7
joxyzan <johan.hagvil@gmail.com>
2026-09-02 12:03:33 +0200

    complaints list added

--- pages/area/panels/complaint-latest.php
+++ pages/area/panels/complaint-latest.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* Compact output of the three latest posts in category 'disappeared' 
+* Compact output of the three latest posts in category 'complaint' 
 **/
 
 // Exit if accessed directly
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Arguments
 $args = array(
     'post_type' => 'post',
-    'category_name' => 'disappeared',
+    'category_name' => 'complaint',
     'posts_per_page' => 3,
 );
 
@@ -20,9 +20,9 @@ $the_query = new WP_Query( $args );
 $count = $the_query->found_posts; ?>
 
 <!--Output-->
-<div class="wrapped link" style="min-width:250px" onclick="location.href='<?php echo get_home_url( null, '/category/disappeared/' ); ?>'">
-<h5>💢 Försvunna saker</h5>
-<p class="small">💡 Saker som inte hittats i skåpet vid hämtning.</p>
+<div class="wrapped link" style="min-width:250px" onclick="location.href='<?php echo get_home_url( null, '/category/complaint/' ); ?>'">
+<h5>💩 Reklamerade saker</h5>
+<p class="small">💡 Saker som har reklamerats av mottagare.</p>
 <p class="small">↓ 3 senaste<span class="right blue">Se alla →</span></p>
 <hr>
 <?php if ( $the_query->have_posts() ) : ?>
@@ -31,7 +31,7 @@ $count = $the_query->found_posts; ?>
 <?php endwhile; ?>
 
 <?php else : ?>
-    <p class="info">💢 Inga försvunna saker.</p>
+    <p class="info">💩 Inga reklamationer.</p>
 <?php endif; ?>
 
 <?php wp_reset_postdata(); ?>
