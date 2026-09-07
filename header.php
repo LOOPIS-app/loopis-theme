8e12147d6570f5720224f3ee761804edd6eb5b11
huberthilborn <Hubert.hilborn@hotmail.com>
2026-09-07 16:19:21 +0200

    fix

--- header.php
+++ header.php
@@ -120,9 +120,9 @@
 	<header id="header">
 		<div class="group">
 			<div class="header-back" onclick="history.back()"><i class="fas fa-chevron-left"></i></div>
-			<a href="<?php echo esc_url(home_url('/start/')); ?>"><img src="<?php echo LOOPIS_THEME_URI; ?>/assets/img/LOOPIS_logo.png" alt="LOOPIS-logo" id="header-logo"></a>
+			<a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo LOOPIS_THEME_URI; ?>/assets/img/LOOPIS_logo.png" alt="LOOPIS-logo" id="header-logo"></a>
 			<div class="header-area">📍<?php echo $area ?></div>
-			<div class="header-faq" onclick="location.href='<?php echo esc_url( network_home_url() ); ?>'">🗺</div>
+			<div class="header-faq" onclick="location.href='<?php echo esc_url( network_home_url('/start/') ); ?>'">🗺</div>
 			</div>
 		</header>
 		
