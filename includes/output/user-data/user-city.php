ca89975cd290fa0d6bb5157bd953f6d5f9589512
joxyzan <johan.hagvil@gmail.com>
2026-09-08 15:22:10 +0200

    include added

--- includes/output/user-data/user-city.php
+++ includes/output/user-data/user-city.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Output user postal area (place of living) based on postal code (zipcode).
+ * Output user postal area (place of living).
  * 
- * If postal area is not yet stored, it will be fetched and stored based on postal code.
+ * If not yet stored in user_meta, it will be fetched and stored based on postal code.
  *
  * $user_id has to be passed from context!
  */
@@ -16,14 +16,12 @@ $city = get_user_meta($user_id, 'wpum_postarea', true);
 
 // Get user postal area
 if(empty($city)){
-    if(function_exists('loopis_get_city')){
+    $city = 'Okänt';
+    // Include function for mapping postal code to postal area.
+    include_once LOOPIS_USERS_DIR . '/includes/functions/loopis-get-city.php';
         $city = loopis_get_city(get_user_meta($user_id, 'wpum_postcode', true));
         update_user_meta($user_id, 'wpum_postarea', $city);
-    }else{
-        $city = 'okänt';
-    }
 }
-
 
 // Output
 echo esc_html($city);
